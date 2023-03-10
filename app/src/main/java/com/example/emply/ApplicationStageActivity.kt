784d3c16b698b4e0292f4b5eb05fb94be11f34f0
th1rdT33N <nathanon.b@kkumail.com>
2023-03-11 05:45:52 +0700

    Application stage

---
+++ app/src/main/java/com/example/emply/ApplicationStageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.emply
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplicationStageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application_stage)
+    }
+}
